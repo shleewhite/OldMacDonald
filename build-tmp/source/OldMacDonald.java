7a7cc795adb27870d6fcfa87e4c71fbcd38044f4
shleewhite <lwhite@sportsbasement.com>
2014-10-24 10:07:26 -0700

    added code

--- build-tmp/source/OldMacDonald.java
+++ build-tmp/source/OldMacDonald.java
@@ -1,11 +1,27 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OldMacDonald extends PApplet {
+
 Farm[] yayFarm;
-void setup()
+public void setup()
 {
 	yayFarm = new Farm[1];
 	yayFarm[0] = new Farm();
   yayFarm[0].animalSounds();
 }
-void draw()
+public void draw()
 {
 	
 }
@@ -56,7 +72,7 @@ class Chick implements Animal
   {         
      myType = type;   
 
-     if (Math.random()<0.5)
+     if (Math.random()<0.5f)
      {
       mySound = sound2;
      }
@@ -102,5 +118,14 @@ class NamedCow extends Cow
     }
     public String getName(){return cowName;}
     public String getSound(){return mySound;}     
-    public String getType(){return myType;} 
+  public String getType(){return myType;} 
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OldMacDonald" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
